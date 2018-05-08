c1e8c67a2f00bbbca06d14bdd0556b020ed4c809
Timm Friebe <thekid@thekid.de>
2018-05-08 23:18:27 +0200

    Initial release

---
+++ src/main/php/web/auth/cas/CasLogin.class.php
@@ -0,0 +1,109 @@
+<?php namespace web\auth\cas;
+
+use web\Cookie;
+use web\Filter;
+use web\Error;
+use peer\http\HttpConnection;
+use xml\XMLFormatException;
+use xml\dom\Document;
+use xml\parser\XMLParser;
+use xml\parser\StreamInputSource;
+use web\session\Sessions;
+
+/**
+ * CAS Login filter
+ *
+ * @see   https://apereo.github.io/cas/4.1.x/protocol/CAS-Protocol-Specification.html
+ * @test  xp://web.auth.cas.unittest.CasLoginTest
+ */
+class CasLogin implements Filter {
+  private $sso, $sessions;
+
+  /** Creates a new instance with a given SSO base url */
+  public function __construct(string $sso, Sessions $sessions) {
+    $this->sso= rtrim($sso, '/');
+    $this->sessions= $sessions;
+  }
+
+  /**
+   * Validates a CAS ticket
+   *
+   * @param  string $ticket
+   * @param  string $service
+   * @return peer.http.HttpResponse
+   */
+  protected function validate($ticket, $service) {
+    return (new HttpConnection($this->sso.'/serviceValidate'))->get([
+      'ticket'  => $ticket,
+      'service' => $service
+    ]);
+  }
+
+  /**
+   * Filter
+   *
+   * @param  web.Request $request
+   * @param  web.Response $response
+   * @param  web.Invocation $invocation
+   * @return var
+   */
+  public function filter($request, $response, $invocation) {
+
+    // Validate ticket, then relocate to self without ticket parameter
+    if ($ticket= $request->param('ticket')) {
+      $service= $request->uri()->using()->param('ticket', null)->create();
+
+      $validate= $this->validate($ticket, $service);
+      if (200 !== $validate->statusCode()) {
+        throw new Error($validate->statusCode(), $validate->message());
+      }
+
+      $result= new Document();
+      try {
+        (new XMLParser())->withCallback($result)->parse(new StreamInputSource($validate->in()));
+      } catch (XMLFormatException $e) {
+        throw new Error(500, 'FORMAT: Validation cannot be parsed', $e);
+      }
+
+      if ($failure= $result->getElementsByTagName('cas:authenticationFailure')) {
+        throw new Error(500, $failure[0]->getAttribute('code').': '.trim($failure[0]->getContent()));
+      } else if (!($success= $result->getElementsByTagName('cas:authenticationSuccess'))) {
+        throw new Error(500, 'UNEXPECTED: '.$result->getSource());
+      }
+
+      $user= ['username' => $result->getElementsByTagName('cas:user')[0]->getContent()];
+      if ($attr= $result->getElementsByTagName('cas:attributes')) {
+        foreach ($attr[0]->getChildren() as $child) {
+          $user[str_replace('cas:', '', $child->getName())]= $child->getContent();
+        }
+      }
+
+      $session= $this->sessions->create();
+      try {
+        $session->register('user', $user);
+        $response->cookie((new Cookie('session', $session->id()))->maxAge($this->sessions->duration()));
+      } finally {
+        $session->close();
+      }
+
+      $response->answer(302);
+      $response->header('Location', $service);
+      return;
+    }
+
+    // Handle session
+    if ($session= $this->sessions->locate($request->cookie('session'))) {
+      try {
+        if ($session->valid()) {
+          $request->pass('user', $session->value('user'));
+          return $invocation->proceed($request, $response);
+        }
+      } finally {
+        $session->close();
+      }
+    }
+
+    $response->answer(302);
+    $response->header('Location', $this->sso.'/login?service='.urlencode($request->uri()));
+  }
+}
---
+++ src/test/php/web/auth/cas/unittest/CasLoginTest.class.php
@@ -0,0 +1,190 @@
+<?php namespace web\auth\cas\unittest;
+
+use io\streams\MemoryInputStream;
+use peer\http\HttpResponse;
+use unittest\TestCase;
+use web\auth\cas\CasLogin;
+use web\Error;
+use web\filters\Invocation;
+use web\io\TestInput;
+use web\io\TestOutput;
+use web\Request;
+use web\Response;
+use web\session\InMemory;
+
+class CasLoginTest extends TestCase {
+  const SSO    = 'https://sso.example.com';
+  const TICKET = 'ST-1856339-aA5Yuvrxzpv8Tau1cYQ7';
+
+  private $sessions;
+
+  /** @return void */
+  public function setUp() {
+    $this->sessions= new InMemory();
+  }
+
+  /**
+   * Creates a validation response
+   *
+   * @param  string $xml
+   * @return peer.http.HttpResponse
+   */
+  public static function response($xml) {
+    return new HttpResponse(new MemoryInputStream(sprintf(
+      "HTTP/1.1 200 OK\r\nContent-Type: text/xml\r\nContent-Length: %d\r\n\r\n%s",
+      strlen($xml),
+      $xml
+    )));
+  }
+
+  /**
+   * Calls filter
+   * 
+   * @param  string $uri
+   * @param  [:string] $headers
+   * @param  web.auth.cas.CasLogin $fixture
+   * @return web.Response
+   */
+  private function filter($uri, $headers, $login) {
+    $req= new Request(new TestInput('GET', $uri, $headers));
+    $res= new Response(new TestOutput());
+    $login->filter($req, $res, new Invocation(function($req, $res) { }));
+    return $res;
+  }
+
+  #[@test]
+  public function can_create() {
+    new CasLogin(self::SSO, $this->sessions);
+  }
+
+  #[@test]
+  public function redirects_to_login() {
+    $res= $this->filter('/', [], new CasLogin(self::SSO, $this->sessions));
+    $this->assertEquals(self::SSO.'/login?service=http%3A%2F%2Flocalhost%2F', $res->headers()['Location']);
+  }
+
+  #[@test]
+  public function validates_ticket_then_redirects_to_self() {
+    $res= $this->filter('/?ticket='.self::TICKET, [], newinstance(CasLogin::class, [self::SSO, $this->sessions], [
+      'validate' => function($service, $ticket) {
+        return CasLoginTest::response('
+          <cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
+            <cas:authenticationSuccess>
+              <cas:user>username</cas:user>
+            </cas:authenticationSuccess>
+          </cas:serviceResponse>
+        ');
+      }
+    ]));
+    $this->assertEquals('http://localhost/', $res->headers()['Location']);
+  }
+
+  #[@test, @expect(class= Error::class, withMessage= '/INVALID_TICKET: Ticket .+ not recognized/')]
+  public function shows_error_when_ticket_cannot_be_validated() {
+    $this->filter('/?ticket='.self::TICKET, [], newinstance(CasLogin::class, [self::SSO, $this->sessions], [
+      'validate' => function($service, $ticket) {
+        return CasLoginTest::response('
+          <cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
+            <cas:authenticationFailure code="INVALID_TICKET">
+              Ticket ST-1856339-aA5Yuvrxzpv8Tau1cYQ7 not recognized`
+            </cas:authenticationFailure>
+          </cas:serviceResponse>
+        ');
+      }
+    ]));
+  }
+
+  #[@test, @expect(class= Error::class, withMessage= '/UNEXPECTED: .+/')]
+  public function shows_error_when_validation_response_invalid() {
+    $this->filter('/?ticket='.self::TICKET, [], newinstance(CasLogin::class, [self::SSO, $this->sessions], [
+      'validate' => function($service, $ticket) {
+        return CasLoginTest::response('
+          <cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
+            <!-- Empty -->
+          </cas:serviceResponse>
+        ');
+      }
+    ]));
+  }
+
+  #[@test, @expect(class= Error::class, withMessage= '/FORMAT: Validation cannot be parsed/')]
+  public function shows_error_when_validation_response_not_well_formed() {
+    $this->filter('/?ticket='.self::TICKET, [], newinstance(CasLogin::class, [self::SSO, $this->sessions], [
+      'validate' => function($service, $ticket) {
+        return CasLoginTest::response('
+          <cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
+          </cas:NOT_WELL_FORMED>
+        ');
+      }
+    ]));
+  }
+
+  #[@test]
+  public function stores_user_in_session() {
+    $this->filter('/?ticket='.self::TICKET, [], newinstance(CasLogin::class, [self::SSO, $this->sessions], [
+      'validate' => function($service, $ticket) {
+        return CasLoginTest::response('
+          <cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
+            <cas:authenticationSuccess>
+              <cas:user>test</cas:user>
+            </cas:authenticationSuccess>
+          </cas:serviceResponse>
+        ');
+      }
+    ]));
+    $this->assertEquals(['username' => 'test'], current($this->sessions->all())->value('user'));
+  }
+
+  #[@test]
+  public function stores_attributes_in_session() {
+    $this->filter('/?ticket='.self::TICKET, [], newinstance(CasLogin::class, [self::SSO, $this->sessions], [
+      'validate' => function($service, $ticket) {
+        return CasLoginTest::response('
+          <cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
+            <cas:authenticationSuccess>
+              <cas:user>test</cas:user>
+              <cas:attributes>
+                <cas:firstname>John</cas:firstname>
+                <cas:lastname>Doe</cas:lastname>
+                <cas:email>jdoe@example.org</cas:email>
+              </cas:attributes>
+            </cas:authenticationSuccess>
+          </cas:serviceResponse>
+        ');
+      }
+    ]));
+    $this->assertEquals(
+      ['username' => 'test', 'firstname' => 'John', 'lastname' => 'Doe', 'email' => 'jdoe@example.org'],
+      current($this->sessions->all())->value('user')
+    );
+  }
+
+  #[@test]
+  public function proceeds_with_invocation_if_logged_in() {
+    $login= new CasLogin(self::SSO, $this->sessions);
+
+    $session= $this->sessions->create();
+    $session->register('user', ['username' => 'test']);
+
+    $this->assertEquals('Invoked', $login->filter(
+      new Request(new TestInput('GET', '/', ['Cookie' => 'session='.$session->id()])),
+      new Response(new TestOutput()),
+      new Invocation(function($req, $res) { return 'Invoked'; }
+    )));
+  }
+
+  #[@test]
+  public function redirects_to_login_if_session_id_non_existant() {
+    $res= $this->filter('/', ['Cookie' => 'session=@does.not.exist@'], new CasLogin(self::SSO, $this->sessions));
+    $this->assertEquals(self::SSO.'/login?service=http%3A%2F%2Flocalhost%2F', $res->headers()['Location']);
+  }
+
+  #[@test]
+  public function redirects_to_login_if_session_invalid() {
+    $session= $this->sessions->create();
+    $session->destroy();
+
+    $res= $this->filter('/', ['Cookie' => 'session='.$session->id()], new CasLogin(self::SSO, $this->sessions));
+    $this->assertEquals(self::SSO.'/login?service=http%3A%2F%2Flocalhost%2F', $res->headers()['Location']);
+  }
+}
